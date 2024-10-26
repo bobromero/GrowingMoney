11cde91fd345381e6e24a976ce424909d1cebfb1
bobromero <rromero123102@gmail.com>
2024-10-26 18:10:40 -0400

    added bullets

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -8,24 +8,28 @@ var closestEnemy:Node2D
 
 
 func _ready() -> void:
+	_getClosestEnemy()
+
+func _process(delta: float) -> void:
+	if closestEnemy != null:
+		velocity = (position - closestEnemy.global_position).normalized() * -bulletSpeed
+		move_and_slide()
+	elif GameManager.NumEnemies() <= 0:
+		queue_free()
+	else:
+		_getClosestEnemy()
+
+func _getEnemies() -> Array[Node]:
+	return get_tree().get_nodes_in_group("Enemy")
+
+func _getClosestEnemy():
 	Enemies.assign(_getEnemies())
-	var closestDist = (Player.playerPos - Enemies[0].position).length()
+	var closestDist = 2100000.0
 	for e:Node2D in Enemies:
 		var dist = (Player.playerPos - e.position).length()
 		if dist < closestDist:
 			closestDist = dist
 			closestEnemy = e
-
-func _process(delta: float) -> void:
-	if closestEnemy != null:
-		velocity = (position - closestEnemy.position) * bulletSpeed
-		move_and_slide()
-	else:
-		queue_free()
-
-func _getEnemies() -> Array[Node]:
-	return get_tree().get_nodes_in_group("Enemy")
-
-
+	
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	pass
